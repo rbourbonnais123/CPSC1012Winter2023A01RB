697d3ecb495a9728ddb4cb1e325c1bee4ad21f21
Robert Bourbonnais <rbourbonnais@nait.ca>
2023-01-18 18:20:50 -0700

    Modified CalculateAcresInTractOfLand

--- CodeSamplesCPSC1012/CalculateAcresInTractOfLand/Program.cs
+++ CodeSamplesCPSC1012/CalculateAcresInTractOfLand/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Declare a constant for the number of square feet in one acre.
+/* 
+ * Declare a constant for the number of square feet in one acre.
+ * When declaring a constant you must also assign the value as part of the declaration.
+ */
 
 const double numberOfSquareFeetInOneAcre = 43560;
 
@@ -9,7 +12,8 @@ const double numberOfSquareFeetInOneAcre = 43560;
  * Pretend the user enters in the value 87120
  */
 
-double squareFeetUserSupplied = 87120;
+double squareFeetUserSupplied;
+squareFeetUserSupplied = 87120;
 
 // Declare a variable the holds the calculated number of acres.
 
@@ -23,4 +27,9 @@ calculatedNumberOfAcres = squareFeetUserSupplied / numberOfSquareFeetInOneAcre;
 //Print out the answer
 
 Console.WriteLine($"The number of acres for the user supplied value is: {calculatedNumberOfAcres}");
+
+/*
+ * Things to be aware of and to try:
+ *      1. why is the data type used double vs int
+ *      2. 
 
