3b58d446e162227c12d722065613fad289535f92
Robert Bourbonnais <rbourbonnais@nait.ca>
2023-01-23 11:47:55 -0700

    Added code.

---
+++ CodeSamplesCPSC1012/CalculateTheMean/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double userNumberOne, userNumberTwo, userNumberThree, calculateMean;
+
+Console.WriteLine("Enter your first value.");
+userNumberOne = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your second value.");
+userNumberTwo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your third value.");
+userNumberThree = double.Parse(Console.ReadLine());
+
+calculateMean = (userNumberOne + userNumberTwo + userNumberThree) / 3;
+
+Console.WriteLine($"Calculate mean was: {calculateMean}");
+
+
