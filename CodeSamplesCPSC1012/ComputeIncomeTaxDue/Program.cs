c0ab9e798a9af9f29cf4472d19c022e76974f236
rbourbonnais123 <rbourbonnais@nait.ca>
2023-02-06 00:41:10 -0700

    Changed code.

--- CodeSamplesCPSC1012/ComputeIncomeTaxDue/Program.cs
+++ CodeSamplesCPSC1012/ComputeIncomeTaxDue/Program.cs
@@ -8,15 +8,15 @@ incomeEntered = double.Parse(Console.ReadLine());
 if (incomeEntered <= 50000)
 {
     taxDue = incomeEntered * .05;
-    Console.WriteLine($"For {incomeEntered} tax due is: {taxDue}");
+    Console.WriteLine($"For {incomeEntered:c} tax due is: {taxDue:c}");
 }
 else if (incomeEntered <= 100000)
 {
     taxDue = 2500 + ( (incomeEntered - 50000) * .07);
-    Console.WriteLine($"For {incomeEntered:f2} tax due is: {taxDue:f2}");
+    Console.WriteLine($"For {incomeEntered:c} tax due is: {taxDue:c}");
 }
 else
 {
     taxDue = 6000 + ((incomeEntered - 100000) * .09);
-    Console.WriteLine($"For {incomeEntered} tax due is: {taxDue}");
+    Console.WriteLine($"For {incomeEntered:c} tax due is: {taxDue:c}");
 }
