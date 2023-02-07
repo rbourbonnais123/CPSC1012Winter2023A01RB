f33978e7e823aab6c80a61fc5c1ecaf381ab1e88
Robert Bourbonnais <rbourbonnais@nait.ca>
2023-02-06 18:19:55 -0700

    Changed code.

--- CodeSamplesCPSC1012/CalculateInternetBill/Program.cs
+++ CodeSamplesCPSC1012/CalculateInternetBill/Program.cs
@@ -1,75 +1,82 @@
-﻿const double PackagePriceA = 10.95,
-    PackagePriceB = 13.95,
-    PackagePriceC = 19.95,
-    OverchargeForPackageA = 2.00,
-    OverchargeForPackageB = 1.00;
-
-const int HoursAllowedForPackageA = 10,
-    HoursAllowedForPackageB = 20;
-
-string servicePackage;
+﻿string servicePackage;
 int hoursUsed;
 double customerBill = 0.00;
 
 Console.WriteLine("Enter the service package indicator (A, B, or C): ");
 servicePackage = Console.ReadLine();
 
-switch (servicePackage)
+if (servicePackage != "C")
 {
-    case "A":
-        customerBill = PackagePriceA;
-        Console.WriteLine("Enter the number of hours used: ");
-        hoursUsed = int.Parse(Console.ReadLine());
-        if (hoursUsed > HoursAllowedForPackageA)
+    Console.WriteLine("Enter hours used: ");
+    hoursUsed = int.Parse(Console.ReadLine());
+
+    if (servicePackage == "A")
+    {
+        customerBill = 9.95;
+        if (hoursUsed > 10)
         {
-            customerBill = PackagePriceA + ((hoursUsed - HoursAllowedForPackageA) * OverchargeForPackageA);
+            customerBill = 9.95 + ((hoursUsed - 10) * 2.00);
         }
-        break;
-    case "B":
-        customerBill = PackagePriceB;
-        Console.WriteLine("Enter the number of hours used: ");
-        hoursUsed = int.Parse(Console.ReadLine());
-        if (hoursUsed > HoursAllowedForPackageB)
+    }
+    else
+    {
+        customerBill = 13.95;
+        if (hoursUsed > 20)
         {
-            customerBill = PackagePriceB + ((hoursUsed - HoursAllowedForPackageB) * OverchargeForPackageB);
+            customerBill = 13.95 + ((hoursUsed - 20) * 1.00);
         }
-        break;
-    case "C":
-        customerBill = PackagePriceC;
-        break;
-    default:
-        Console.WriteLine("Unable to calculate customer bill package (A, B, or C) was not entered.");
-        break;
+    }
 }
+else
+{
+    customerBill = 19.95;
+}
+
 
 Console.WriteLine($"Customer bill total is: {customerBill:c}");
 
-//if (servicePackage != "C")
-//{
-//    Console.WriteLine("Enter hours used: ");
-//    hoursUsed = int.Parse(Console.ReadLine());
 
-//    if (servicePackage == "A")
-//    {
-//        customerBill = 9.95;
-//        if (hoursUsed > 10)
-//        {
-//            customerBill = 9.95 + ((hoursUsed - 10) * 2.00);
-//        }
-//    }
-//    else
-//    {
-//        customerBill = 13.95;
-//        if (hoursUsed > 20)
-//        {
-//            customerBill = 13.95 + ((hoursUsed - 20) * 1.00);
-//        }
-//    }
-//}
-//else
+
+//const double PackagePriceA = 10.95,
+//    PackagePriceB = 13.95,
+//    PackagePriceC = 19.95,
+//    OverchargeForPackageA = 2.00,
+//    OverchargeForPackageB = 1.00;
+
+//const int HoursAllowedForPackageA = 10,
+//    HoursAllowedForPackageB = 20;
+
+//switch (servicePackage)
 //{
-//    customerBill = 19.95;
+//    case "A":
+//        customerBill = PackagePriceA;
+//        Console.WriteLine("Enter the number of hours used: ");
+//        hoursUsed = int.Parse(Console.ReadLine());
+//        if (hoursUsed > HoursAllowedForPackageA)
+//        {
+//            customerBill = PackagePriceA + ((hoursUsed - HoursAllowedForPackageA) * OverchargeForPackageA);
+//        }
+//        break;
+//    case "B":
+//        customerBill = PackagePriceB;
+//        Console.WriteLine("Enter the number of hours used: ");
+//        hoursUsed = int.Parse(Console.ReadLine());
+//        if (hoursUsed > HoursAllowedForPackageB)
+//        {
+//            customerBill = PackagePriceB + ((hoursUsed - HoursAllowedForPackageB) * OverchargeForPackageB);
+//        }
+//        break;
+//    case "C":
+//        customerBill = PackagePriceC;
+//        break;
+//    default:
+//        Console.WriteLine("Unable to calculate customer bill package (A, B, or C) was not entered.");
+//        break;
 //}
+
+
+
+
 
 
 
