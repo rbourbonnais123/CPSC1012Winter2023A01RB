7a98c3588a62efd1812ed5f0a15ac0422e56f869
Robert Bourbonnais <rbourbonnais@nait.ca>
2023-02-08 13:43:04 -0700

    Changed code.

---
+++ CodeSamplesCPSC1012/TotalSales/Program.cs
@@ -0,0 +1,18 @@
+﻿int days; 
+double sales; 
+double totalSales = 0.00; 
+
+Console.Write("For how many days do you have sales figures for? ");
+days = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int count = 1; count <= days; count++)
+{
+    Console.Write($"Enter the sales figure for day #{count}: ");
+    sales = double.Parse(Console.ReadLine());
+    totalSales += sales;
+}
+
+Console.WriteLine($"\nTotal sales were: {totalSales:c}");
+
